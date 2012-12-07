5e6e85b69729af59ebbb0f56be8c65407a6e524b
Sebastien Binet <binet@cern.ch>
2012-12-07 17:15:36 +0100

    update for multi-proj

---
+++ proj-c/pkg/control/rootcomps-c/src/th1d.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "rootcomps/test-h1d.hh"
+
+void my_test_th1d() 
+{
+  std::cout << "=== from proj-c ===\n";
+  test_th1d();
+  std::cout << "=== from proj-c === [done]\n";
+}
+
